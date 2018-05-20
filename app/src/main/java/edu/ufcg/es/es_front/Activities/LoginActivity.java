c43884d8bdeccaffe6106b77b676cc8d0c1fc575
AdsonSilva <adson.silva@ccc.ufcg.edu.br>
2018-05-20 17:47:11 -0300

    Login with google

--- app/src/main/java/edu/ufcg/es/es_front/Activities/LoginActivity.java
+++ app/src/main/java/edu/ufcg/es/es_front/Activities/LoginActivity.java
@@ -3,13 +3,51 @@ package edu.ufcg.es.es_front.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 import edu.ufcg.es.es_front.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    private void updateUI(GoogleSignInAccount account){
+
+        //ninguém logado, carregar tela de login
+        if(account == null){
+
+        }
+        //alguém já logado, redirecionar para tela de home
+        else{
+
+        }
+
     }
 }
