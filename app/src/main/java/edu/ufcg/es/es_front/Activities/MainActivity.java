a4bdf4fa3e3d1479b3aed556c83d1d5f0fe74e98
AdsonSilva <adson.silva@ccc.ufcg.edu.br>
2018-06-17 23:28:09 -0300

    Refactoring MainActivity

--- app/src/main/java/edu/ufcg/es/es_front/Activities/MainActivity.java
+++ app/src/main/java/edu/ufcg/es/es_front/Activities/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.ufcg.es.es_front.Activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -16,34 +18,46 @@ import android.view.View;
 
 import edu.ufcg.es.es_front.R;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private FloatingActionButton floatingActionButton;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Context context;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        this.init();
+        this.setListeners();
 
-        @Override
-        public void onClick(View view) {
-         Intent createVehicleIntent = new Intent(view.getContext(), CreateVehicleActivity.class);
-                startActivity(createVehicleIntent);
-            }
-        });
+        setSupportActionBar(this.toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+
+
+    }
+
+    private void init(){
+        this.toolbar = findViewById(R.id.toolbar);
+        this.floatingActionButton = findViewById(R.id.fab);
+        this.drawerLayout = findViewById(R.id.drawer_layout);
+        this.navigationView = findViewById(R.id.nav_view);
+
+        this.context = this.getApplicationContext();
+    }
+
+    private void setListeners() {
+        this.floatingActionButton.setOnClickListener(this.floatActionButtonClickListener());
+        navigationView.setNavigationItemSelectedListener(this.navigationItemSelectedListener());
     }
 
     @Override
@@ -79,22 +93,34 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+    private NavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener(){
+        return new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
 
-        if (id == R.id.nav_manage) {
-            //redireciona pra tela de configuracoes
+                if (id == R.id.nav_manage) {
+                    //redireciona pra tela de configuracoes
 
-        } else if (id == R.id.nav_send) {
-            Intent aboutIntent = new Intent(this, AboutActivity.class);
-            startActivity(aboutIntent);
-        }
+                } else if (id == R.id.nav_send) {
+                    Intent aboutIntent = new Intent(context, AboutActivity.class);
+                    startActivity(aboutIntent);
+                }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        };
+    }
+
+    private View.OnClickListener floatActionButtonClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent createVehicleIntent = new Intent(context, CreateVehicleActivity.class);
+                startActivity(createVehicleIntent);
+            }
+        };
     }
 }
