743b9133943f665248e90bf1e0962af32c9be26f
matheusyhwh <matheusyhwh@gmail.com>
2018-07-22 13:21:47 -0300

    correção serviceslist e campos de createService

--- app/src/main/java/edu/ufcg/es/es_front/Activities/ServiceListActivity.java
+++ app/src/main/java/edu/ufcg/es/es_front/Activities/ServiceListActivity.java
@@ -1,27 +1,18 @@
 package edu.ufcg.es.es_front.Activities;
 
-import android.content.Context;
-import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import edu.ufcg.es.es_front.adapters.CarsListAdapter;
 import edu.ufcg.es.es_front.adapters.ServicesListAdapter;
 import edu.ufcg.es.es_front.controllers.UserController;
 import edu.ufcg.es.es_front.httpClient.RequestQueueSingleton;
 import edu.ufcg.es.es_front.httpClient.callbacks.OnGetServicesByCarCallback;
 import edu.ufcg.es.es_front.httpClient.requests.GetServicesByCarRequest;
-import edu.ufcg.es.es_front.models.Car;
 import edu.ufcg.es.es_front.models.Service;
 import edu.ufcg.es.es_front.utils.ActivityUtils;
 import edu.ufcg.es.es_front.R;
@@ -29,9 +20,6 @@ import edu.ufcg.es.es_front.R;
 public class ServiceListActivity extends AppCompatActivity {
 
     private ListView servicesListView;
-    private Context context;
-    private ServicesListAdapter adapter;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,13 +33,12 @@ public class ServiceListActivity extends AppCompatActivity {
 
     private void init() {
         this.servicesListView = findViewById(R.id.listViewServices);
-        this.context = this.getApplicationContext();
     }
 
     private void getServices(){
-        ActivityUtils.showProgressDialog(this, "Getting services");
+        ActivityUtils.showProgressDialog(this, "Getting cars");
         Map<String, String> headers = new HashMap<>();
-        headers.put("Authorization", "bearer " + UserController.getUserLogged().getToken());
+        headers.put("authorization", "bearer " + UserController.getUserLogged().getToken());
         GetServicesByCarRequest getServicesByCarRequest = new GetServicesByCarRequest(getServicesByCarCallback());
         RequestQueueSingleton.getInstance(this).addToRequestQueue(getServicesByCarRequest.getRequest(headers));
     }
@@ -60,7 +47,7 @@ public class ServiceListActivity extends AppCompatActivity {
         return new OnGetServicesByCarCallback() {
             @Override
             public void onGetServicesByCarCallbackSucess(ArrayList<Service> response) {
-                adapter = new ServicesListAdapter(response, ServiceListActivity.this);
+                ServicesListAdapter adapter = new ServicesListAdapter(response, ServiceListActivity.this);
                 servicesListView.setAdapter(adapter);
                 ActivityUtils.cancelProgressDialog();
             }
@@ -72,41 +59,6 @@ public class ServiceListActivity extends AppCompatActivity {
             }
         };
     }
-    private View.OnClickListener floatActionButtonClickListener() {
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent createServiceIntent = new Intent(context, CreateServiceActivity.class);
-                startActivity(createServiceIntent);
-            }
-        };
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.user_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_about) {
-            Intent aboutIntent = new Intent(this, AboutActivity.class);
-            startActivity(aboutIntent);
-        } else if(id == R.id.action_logout) {
-            UserController.logout(this);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
 
 }
-
--- app/src/main/java/edu/ufcg/es/es_front/models/Service.java
+++ app/src/main/java/edu/ufcg/es/es_front/models/Service.java
@@ -4,25 +4,15 @@ import java.util.Date;
 
 public class Service {
 
-
-    private String _id, name, price, actualOdometer, description;
+    private String name, price, actualOdometer, description;
     private Date date;
 
-    public Service(String _id, String name, String price, Date date, String actualOdometer, String description) {
-        this._id = _id;
+    public Service(String name, String price, Date date, String actualOdometer, String description) {
         this.name = name;
         this.price = price;
         this.date = date;
         this.actualOdometer = actualOdometer;
         this.description = description;
-    }
-
-    public String get_id() {
-        return _id;
-    }
-
-    public void set_id(String _id) {
-        this._id = _id;
     }
 
     public String getName() {
@@ -63,16 +53,5 @@ public class Service {
 
     public void setDate(Date date) {
         this.date = date;
-    }
-
-    @Override
-    public String toString() {
-        return "Service{" +
-                "_id='" + _id + '\'' +
-                ", name='" + name + '\'' +
-                ", price='" + price + '\'' +
-                ", actualOdometer='" + actualOdometer + '\'' +
-                ", description='" + description + '\'' +
-                '}';
     }
 }
